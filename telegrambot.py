bd03a8e3c4228ca5c4617f6bf5d471cedae4e7ac
abangajoe <139495180+abangajoe@users.noreply.github.com>
2024-07-27 15:27:16 +0000

    Update telegrambot.py

--- telegrambot.py
+++ telegrambot.py
@@ -5,6 +5,7 @@ from keep_alive import keep_alive
 
 keep_alive()
 
+TOKEN = '7135600634:AAGiMAyfJ5HCnAqaVTcQH4Zj3GzhH10_szU'
 bot = Bot(token=os.environ.get('TOKEN'))
 updater = Updater(bot=bot, use_context=True)
 dispatcher = updater.dispatcher
